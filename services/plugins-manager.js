858250f0dfbb52d076c5cf13bbdc0131d65dc84f
Aymane Shoru <chourouaymane@gmail.com>
2025-12-14 09:08:58 +0100

    removing globalThis from the entire project

--- services/plugins-manager.js
+++ services/plugins-manager.js
@@ -1,28 +1,29 @@
 import { sep } from "path"
 
+const pluginsMap = new Map();
+const autoPluginsMap = new Map();
+
+export const getPlugins = () => pluginsMap;
+export const getAutoPlugins = () => autoPluginsMap;
+
 export const addPlugins = (items) => {
   if (!items) return;
   const plugins = items.filter(i => !i.isAuto);
   const autoPlugins = items.filter(i => i.isAuto);
 
-  if (globalThis.PLUGINS && globalThis.AUTOPLUGINS) {
-    plugins.forEach(i => globalThis.PLUGINS.set(i.pluginId, i));
-    autoPlugins.forEach(i => globalThis.AUTOPLUGINS.set(i.pluginId, i));
-  } else {
-    globalThis.PLUGINS = new Map(plugins.map(i => [i.pluginId, i]))
-    globalThis.AUTOPLUGINS = new Map(autoPlugins.map(i => [i.pluginId, i]))
-  }
+  plugins.forEach(i => pluginsMap.set(i.pluginId, i));
+  autoPlugins.forEach(i => autoPluginsMap.set(i.pluginId, i));
 }
 
 const _removePluginsWhere = (condition) => {
-  globalThis.PLUGINS.forEach((plugin, pluginId) => {
+  pluginsMap.forEach((plugin, pluginId) => {
     if (condition(plugin))
-      globalThis.PLUGINS.delete(pluginId);
+      pluginsMap.delete(pluginId);
   })
   
-  globalThis.AUTOPLUGINS.forEach((plugin, pluginId) => {
+  autoPluginsMap.forEach((plugin, pluginId) => {
     if (condition(plugin))
-      globalThis.AUTOPLUGINS.delete(pluginId);
+      autoPluginsMap.delete(pluginId);
   })
 }
 
